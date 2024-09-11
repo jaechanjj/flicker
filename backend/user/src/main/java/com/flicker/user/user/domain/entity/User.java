241a61c89fe128967c542d3e13fe20c0d0e2e835
오동규 <ehdrb700606@naver.com>
2024-09-12 00:24:46 +0900

    [feat] user entity 생성

--- backend/user/src/main/java/com/flicker/user/user/domain/entity/User.java
+++ backend/user/src/main/java/com/flicker/user/user/domain/entity/User.java
@@ -1,11 +1,13 @@
 package com.flicker.user.user.domain.entity;
 
+import com.flicker.user.user.domain.vo.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
@@ -15,17 +17,26 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer memberSeq;
+    private Long memberSeq;
 
     @Column(nullable = false)
     private String userName;
-
     private String email;
     private String hashedPass;
-    private String fullName;
+//    private String fullName;
     private String nickname;
     private LocalDate birthDate;
-    private Integer age;
-    private String gender;
-    private DateTime
+//    private Integer age;
+    private Character gender;
+    private String profilePhotoUrl;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Integer isActive;
+
+    /*
+        role을 반드시 가져야 한다.
+     */
+    @Embedded
+    private Role role;
 }
