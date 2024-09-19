d24832f8f4964332eb6f2888de64d9305dd13a76
오동규 <ehdrb700606@naver.com>
2024-09-19 17:38:31 +0900

    [feat] User Entity 작성

--- backend/user/src/main/java/com/flicker/user/user/domain/entity/User.java
+++ backend/user/src/main/java/com/flicker/user/user/domain/entity/User.java
@@ -5,6 +5,7 @@ import com.flicker.user.common.status.StatusCode;
 import com.flicker.user.common.util.HashUtil;
 import com.flicker.user.user.domain.UserGrade;
 import com.flicker.user.user.domain.vo.Role;
+import com.flicker.user.user.domain.vo.UserInfo;
 import com.flicker.user.user.dto.MovieSeqListDto;
 import com.flicker.user.user.dto.UserAndMovieIdDto;
 import com.flicker.user.user.dto.UserUpdateDto;
@@ -13,6 +14,7 @@ import jdk.jshell.Snippet;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -30,34 +32,60 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userSeq;
 
+    @Embedded
+    private UserInfo userInfo;
+    @Embedded
+    private Role role;
+
     private String nickname;
     private String email;
     private String hashedPass;
-    private LocalDate birthDate;
-    private Character gender;
     private String profilePhotoUrl;
 
     @OneToMany(mappedBy = "userMovieId.userSeq")
     private List<FavoriteMovie> favoriteMovies;
-
     @OneToMany(mappedBy = "userMovieId.userSeq")
     private List<BookmarkMovie> bookmarkMovies;
-
     @OneToMany(mappedBy = "userMovieId.userSeq")
     private List<UnlikeMovie> unlikeMovies;
 
-    @Embedded
-    private Role role;
+
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private Integer isActive;
 
+    public void addBookmarkMovie(MovieSeqListDto dto) {
+        for(Long movieSeq : dto.getMovieIdList()){
+            if(isDuplicateFavoriteMovie(movieSeq)){
+                continue;
+            }
+            FavoriteMovie favoriteMovie =  new FavoriteMovie(new UserAndMovieIdDto(movieSeq, this.userSeq));
+            favoriteMovies.add(favoriteMovie);
+        }
+    }
+
+    public boolean isDuplicateFavoriteMovie(Long movieSeq){
+        for(FavoriteMovie favoriteMovie : favoriteMovies){
+            if(favoriteMovie.getUserMovieId().getMovieSeq().equals(movieSeq)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeFavoriteMovie(Long movieSeq){
+        if(isDuplicateFavoriteMovie(movieSeq)){
+            favoriteMovies.remove(movieSeq);
+        }
+    }
+
     public void addFavoriteMovie(MovieSeqListDto dto){
 
         for(Long movieSeq : dto.getMovieIdList()){
-
-//            if(favoriteMovies.con)
-
+            if(isDuplicateFavoriteMovie(movieSeq)){
+                continue;
+            }
             FavoriteMovie favoriteMovie =  new FavoriteMovie(new UserAndMovieIdDto(movieSeq, this.userSeq));
             favoriteMovies.add(favoriteMovie);
         }
@@ -81,11 +109,11 @@ public class User {
     }
 
     public Integer getAge(){
-        if(this.birthDate == null){
+        if(this.userInfo.getBirthDate() == null){
             throw new NoSuchElementException();
         }
         LocalDate now = LocalDate.now();
-        return Period.between(this.birthDate, now).getYears();
+        return Period.between(this.userInfo.getBirthDate(), now).getYears();
     }
 
     protected User() {
@@ -95,11 +123,14 @@ public class User {
         if(birthDate == null || email == null || gender == null || hashedPass == null || nickname == null){
             throw new RestApiException(StatusCode.VALUE_CANT_NULL,null);
         }
-        this.birthDate = birthDate;
-        this.email = email;
-        this.gender = gender;
-        this.hashedPass = HashUtil.sha256(hashedPass);
+        this.userInfo = UserInfo
+                .builder()
+                .birthDate(birthDate)
+                .gender(gender)
+                .build();
         this.nickname = nickname;
+        this.email = email;
+        this.hashedPass = hashedPass;
         this.role = new Role(UserGrade.USER);
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
