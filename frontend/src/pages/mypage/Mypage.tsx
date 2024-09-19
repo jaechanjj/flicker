bb40b25f3c4d04ecb4a06aacf414fdcd9080cbad
조현정 <whguswjd456@naver.com>
2024-09-19 09:42:28 +0900

    [feat] photobook page 구현

--- frontend/src/pages/mypage/Mypage.tsx
+++ frontend/src/pages/mypage/Mypage.tsx
@@ -5,7 +5,6 @@ import { Outlet } from "react-router-dom"; // Outlet을 가져옵니다.
 const Mypage: React.FC = () => {
   return (
     <div>
-      <h1>Mypage</h1>
       {/* 자식 경로가 렌더링될 위치 */}
       <Outlet />
     </div>
