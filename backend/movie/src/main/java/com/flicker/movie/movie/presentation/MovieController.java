af65de0369e10340cb7645451648019fbe078913
rinch12332 <rinch12332@gmail.com>
2024-09-23 11:49:30 +0900

    [feat] 영화도메인 서버 페이징 처리

--- backend/movie/src/main/java/com/flicker/movie/movie/presentation/MovieController.java
+++ backend/movie/src/main/java/com/flicker/movie/movie/presentation/MovieController.java
@@ -6,6 +6,7 @@ import com.flicker.movie.movie.application.ActorService;
 import com.flicker.movie.movie.application.MovieService;
 import com.flicker.movie.movie.dto.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -68,38 +69,38 @@ public class MovieController {
         return ResponseDto.response(StatusCode.SUCCESS, "영화 배우 수정 성공");
     }
 
-    // 영화 상세 정보 조회
-    @GetMapping("/detail/{movieSeq}/{userSeq}")
-    public ResponseEntity<ResponseDto> getMovieDetail(@PathVariable int movieSeq, @PathVariable int userSeq) {
-        MovieDetailResponse response = movieService.getMovieDetail(movieSeq, userSeq);
-        return ResponseDto.response(StatusCode.SUCCESS, response);
-    }
-
     // 전체 영화 목록 조회
-    @GetMapping("/list")
-    public ResponseEntity<ResponseDto> getAllMovieList() {
-        List<MovieListResponse> response = movieService.getAllMovieList();
+    @GetMapping("/list/{page}/{size}")
+    public ResponseEntity<ResponseDto> getAllMovieList(@PathVariable int page, @PathVariable int size) {
+        List<MovieListResponse> response = movieService.getAllMovieList(page, size);
         return ResponseDto.response(StatusCode.SUCCESS, response);
     }
 
     // 장르별 영화 목록 조회
-    @GetMapping("/list/genre/{genre}")
-    public ResponseEntity<ResponseDto> getMovieListByGenre(@PathVariable String genre) {
-        List<MovieListResponse> response = movieService.getMovieListByGenre(genre);
+    @GetMapping("/list/genre/{genre}/{page}/{size}")
+    public ResponseEntity<ResponseDto> getMovieListByGenre(@PathVariable String genre, @PathVariable int page, @PathVariable int size) {
+        List<MovieListResponse> response = movieService.getMovieListByGenre(genre, page, size);
         return ResponseDto.response(StatusCode.SUCCESS, response);
     }
 
     // 배우별 영화 목록 조회
-    @GetMapping("/list/actor/{actorName}")
-    public ResponseEntity<ResponseDto> getMovieListByActor(@PathVariable String actorName) {
-        List<MovieListResponse> response = movieService.getMovieListByActor(actorName);
+    @GetMapping("/list/actor/{actorName}/{page}/{size}")
+    public ResponseEntity<ResponseDto> getMovieListByActor(@PathVariable String actorName, @PathVariable int page, @PathVariable int size) {
+        List<MovieListResponse> response = movieService.getMovieListByActor(actorName, page, size);
         return ResponseDto.response(StatusCode.SUCCESS, response);
     }
 
     // 검색 영화 목록 조회
-    @GetMapping("/list/search/{keyword}/{userSeq}")
-    public ResponseEntity<ResponseDto> getMovieListByKeyword(@PathVariable String keyword, @PathVariable int userSeq) {
-        List<MovieListResponse> response = movieService.getMovieListByKeyword(keyword, userSeq);
+    @GetMapping("/list/search/{keyword}/{userSeq}/{page}/{size}")
+    public ResponseEntity<ResponseDto> getMovieListByKeyword(@PathVariable String keyword, @PathVariable int userSeq, @PathVariable int page, @PathVariable int size) {
+        List<MovieListResponse> response = movieService.getMovieListByKeyword(keyword, userSeq, page, size);
+        return ResponseDto.response(StatusCode.SUCCESS, response);
+    }
+
+    // 영화 상세 정보 조회
+    @GetMapping("/detail/{movieSeq}/{userSeq}")
+    public ResponseEntity<ResponseDto> getMovieDetail(@PathVariable int movieSeq, @PathVariable int userSeq) {
+        MovieDetailResponse response = movieService.getMovieDetail(movieSeq, userSeq);
         return ResponseDto.response(StatusCode.SUCCESS, response);
     }
 
