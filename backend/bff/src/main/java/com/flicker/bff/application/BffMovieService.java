681836e66a337f35d78ce0dff7988bbb7d9845cd
rinch12332 <rinch12332@gmail.com>
2024-09-25 15:29:23 +0900

    [feat] TODO 설정

--- backend/bff/src/main/java/com/flicker/bff/application/BffMovieService.java
+++ backend/bff/src/main/java/com/flicker/bff/application/BffMovieService.java
@@ -91,7 +91,6 @@ public class BffMovieService {
         return util.sendGetRequest(movieBaseUrl, path);
     }
 
-    // TODO: 키워드 기반 검색 결과 및 추천
     public ResponseEntity<ResponseDto> getMovieListBySearch(String keyword, int userSeq, int page, int size) {
         // 1. 외부 API의 경로를 설정합니다.
         String path = util.getUri("/list/search/" + keyword + "/" + userSeq + "/" + page + "/" + size);
@@ -99,6 +98,7 @@ public class BffMovieService {
         return util.sendGetRequest(movieBaseUrl, path);
     }
 
+    // TODO: 영화 상세조회 + 연관 영화 추천  + 좋아요 높은 리뷰 조회
     public ResponseEntity<ResponseDto> getMovieDetail(int movieSeq, int userSeq) {
         // 1. 외부 API의 경로를 설정합니다.
         String path = util.getUri("/detail/" + movieSeq + "/" + userSeq);
@@ -106,33 +106,39 @@ public class BffMovieService {
         return util.sendGetRequest(movieBaseUrl, path);
     }
     
-    // TODO: 경로 수정
+    // TODO: 사용자 행동 기반 영화 추천
     public ResponseEntity<ResponseDto> getActionRecommendationList(int userSeq) {
-        // 1. 배치 서버에서 추천된 영화 번호 목록을 가져옵니다. 
-        String path = util.getUri("/list/recommendation/action/" + userSeq);
-        List<Integer> movieSeqList = (List<Integer>) util.sendGetRequest(batchBaseUrl, path).getBody().getData();
-        // 2. 해당 영화 번호 목록에 해당 하는 영화 목록을 가져옵니다.
-        path = util.getUri("/list/recommendation");
-        return util.sendPostRequest(movieBaseUrl, path, movieSeqList);
+        // 1. 영화 서버에서 사용자의 최근 행동을 가져옴 (10개)
+
+        // 2. 추천 서버로 사용자의 최근 행동 목록을 전송하고, 추천 영화 목록을 가져옴
+
+        // 3. 해당 영화 번호 목록에 해당 하는 영화 목록을 가져와 반환
+        String path = util.getUri("/list/recommendation");
+//        return util.sendPostRequest(movieBaseUrl, path, movieSeqList);
+        return null;
     }
     
-    // TODO: 경로 수정
+    // TODO: 사용자 평점-리뷰 기반 영화 추천
     public ResponseEntity<ResponseDto> getReviewRecommendationList(int userSeq) {
-        // 1. 배치 서버에서 추천된 영화 번호 목록을 가져옵니다. TODO: 경로 수정
-        String path = util.getUri("/list/recommendation/review/" + userSeq);
-        List<Integer> movieSeqList = (List<Integer>) util.sendGetRequest(batchBaseUrl, path).getBody().getData();
-        // 2. 해당 영화 번호 목록에 해당 하는 영화 목록을 가져옵니다.
-        path = util.getUri("/list/recommendation");
-        return util.sendPostRequest(movieBaseUrl, path, movieSeqList);
+        // 1. 유저-리뷰 서버에서 사용자의 리뷰 목록을 가져옴
+
+        // 2. 추천 서버로 사용자의 리뷰 목록을 전송하고, Top-N 사용자 리스트를 가져옴
+
+        // 3. 유저-리뷰 서버에서 Top-N 사용자의 리뷰 감성 점수 목록을 가져옴
+
+        // 4. 추천 서버로 사용자의 리뷰 목록과 Top-N 사용자의 리뷰 감성 점수 목록을 전송하고, 추천 영화 목록을 가져옴
+
+        // 5. 해당 영화 번호 목록에 해당 하는 영화 목록을 가져와 반환
+        String path = util.getUri("/list/recommendation");
+//        return util.sendPostRequest(movieBaseUrl, path, movieSeqList);
+        return null;
     }
     
-    // TODO: 경로 수정
+    // TODO: 1일 기준 TOP 10 영화 목록 조회
     public ResponseEntity<ResponseDto> getTopMovieList() {
-        // 1. 배치 서버에서 추천된 영화 번호 목록을 가져옵니다. TODO: 경로 수정
-        String path = util.getUri("/list/recommendation/top10");
-        List<Integer> movieSeqList = (List<Integer>) util.sendGetRequest(batchBaseUrl, path).getBody().getData();
-        // 2. 해당 영화 번호 목록에 해당 하는 영화 목록을 가져옵니다.
-        path = util.getUri("/list/recommendation");
-        return util.sendPostRequest(movieBaseUrl, path, movieSeqList);
+        // 1. 외부 API 경로 설정
+        String path = util.getUri("/list/top10");
+        // 2. GET 요청 메서드를 사용하여 외부 API에 요청을 보냅니다.
+        return util.sendGetRequest(movieBaseUrl, path);
     }
 }
