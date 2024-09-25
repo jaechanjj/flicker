b0a7b70907d5c635b831912828eae365d707d315
오동규 <ehdrb700606@naver.com>
2024-09-25 11:36:05 +0900

    [fix] Authorizaion 헤더를 클라이언트에서 볼 수 있도록 WebConfig의 exposeHeader설정

--- backend/user/src/main/java/com/flicker/user/common/config/WebConfig.java
+++ backend/user/src/main/java/com/flicker/user/common/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://192.168.30.180:5173", "http://localhost:5173", "http://j11e206.p.ssafy.io:5173")  // 특정 IP와 포트 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")  // 모든 헤더 허용
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);  // 자격 증명(쿠키 등) 허용
     }
 }
