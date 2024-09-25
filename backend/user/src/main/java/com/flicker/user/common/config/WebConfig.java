be0ca228e408ab354646652b5e8588f881c21f33
오동규 <ehdrb700606@naver.com>
2024-09-25 09:54:12 +0900

    [feat] Spring Security, security filter에서 CORS 설정

--- backend/user/src/main/java/com/flicker/user/common/config/WebConfig.java
+++ backend/user/src/main/java/com/flicker/user/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOrigins("http://192.168.30.180:5173", "http://localhost:5173")  // 특정 IP와 포트 허용
+                .allowedOrigins("http://192.168.30.180:5173", "http://localhost:5173", "http://j11e206.p.ssafy.io:5173")  // 특정 IP와 포트 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 자격 증명(쿠키 등) 허용
