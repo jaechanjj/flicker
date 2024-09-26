f08bd8728e70040353b23195a7ea60ae3e4ef5aa
오동규 <ehdrb700606@naver.com>
2024-09-26 17:55:23 +0900

    [feat] review API 작성

--- backend/user/src/main/java/com/flicker/user/review/application/ReviewService.java
+++ backend/user/src/main/java/com/flicker/user/review/application/ReviewService.java
@@ -3,14 +3,17 @@ package com.flicker.user.review.application;
 import com.flicker.user.common.exception.RestApiException;
 import com.flicker.user.common.status.StatusCode;
 import com.flicker.user.review.domain.entity.Review;
+import com.flicker.user.review.dto.AddLikeReviewReqDto;
 import com.flicker.user.review.dto.DeleteReviewReqDto;
 import com.flicker.user.review.dto.RegisterReviewReqDto;
+import com.flicker.user.review.dto.RemoveLikeReviewReqDto;
 import com.flicker.user.review.infrastructure.ReviewRepository;
 import com.flicker.user.user.domain.entity.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,16 +26,17 @@ public class ReviewService {
     public boolean registerReview(RegisterReviewReqDto dto){
 
         Review review = reviewRepository.findByUserSeqAndMovieSeq(dto.getUserSeq(), dto.getMovieSeq());
-        if(review == null){
+        if(review != null){
             throw new RestApiException(StatusCode.DUPLICATED_REVIEW);
         }
+
+        new Review()
         reviewRepository.save(review);
         return true;
     }
 
     @Transactional
     public boolean deleteReview(DeleteReviewReqDto dto){
-        Optional<Review> byId = reviewRepository.findById(dto.getReviewSeq());
 
         Review review = reviewRepository.findById(dto.getReviewSeq())
                 .orElseThrow(() -> new RestApiException(StatusCode.CAN_NOT_FIND_REVIEW));
@@ -40,5 +44,28 @@ public class ReviewService {
         reviewRepository.delete(review);
 
         return true;
+    }
+
+    @Transactional
+    public boolean addLikeReview(AddLikeReviewReqDto dto){
+        Review review = reviewRepository.findById(dto.getLikeReviewSeq())
+                .orElseThrow(() -> new RestApiException(StatusCode.CAN_NOT_FIND_REVIEW));
+
+        return review.addLikeReview(dto.getUserSeq());
+    }
+
+    @Transactional
+    public boolean removeLikeReview(RemoveLikeReviewReqDto dto){
+        Review review = reviewRepository.findById(dto.getLikeReviewSeq())
+                .orElseThrow(() -> new RestApiException(StatusCode.CAN_NOT_FIND_REVIEW));
+        return review.removeLikeReview(dto.getUserSeq());
+    }
+
+    public List<Review> getMovieReviews(Integer movieSeq){
+        return reviewRepository.findAllByMovieSeq(movieSeq);
+    }
+
+    public List<Review> getUserReviews(Integer userSeq){
+        return reviewRepository.findAllByUserSeq(userSeq);
     }
 }
