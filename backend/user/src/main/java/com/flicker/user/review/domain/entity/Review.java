a41e715bf609a53450c8fdef1e1604841a79000f
오동규 <ehdrb700606@naver.com>
2024-09-26 14:40:04 +0900

    [feat] review 엔티티 작성

--- backend/user/src/main/java/com/flicker/user/review/domain/entity/Review.java
+++ backend/user/src/main/java/com/flicker/user/review/domain/entity/Review.java
@@ -1,9 +1,27 @@
 package com.flicker.user.review.domain.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
-//@Entity
-//public class Review {
-//
-//
-//}
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewSeq;
+
+    private Integer userSeq;
+    private Integer movieSeq;
+    private Double reviewRating;
+    private String content;
+    private LocalDateTime createdAt;
+    private Integer isSpoiler;
+    private Boolean isActive;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LikeReview> likeReviews;
+
+
+}
