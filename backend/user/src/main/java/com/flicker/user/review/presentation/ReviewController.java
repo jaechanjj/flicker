3258ddde251078d540620636259322eb43a7fb4f
오동규 <ehdrb700606@naver.com>
2024-09-27 17:34:44 +0900

    [fix] URL 수정

--- backend/user/src/main/java/com/flicker/user/review/presentation/ReviewController.java
+++ backend/user/src/main/java/com/flicker/user/review/presentation/ReviewController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user-review/review")
+@RequestMapping("/api/user/review")
 @RequiredArgsConstructor
 public class ReviewController {
 
