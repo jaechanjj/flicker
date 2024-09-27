b40909cd3ed34298ce888ab68ff2bfad59d9c47b
오동규 <ehdrb700606@naver.com>
2024-09-27 17:24:08 +0900

    [feat] api 요청 주소 수정, DTO 수정

--- backend/bff/src/main/java/com/flicker/bff/dto/user/UserRegisterReqDto.java
+++ backend/bff/src/main/java/com/flicker/bff/dto/user/UserRegisterReqDto.java
@@ -1,4 +1,17 @@
 package com.flicker.bff.dto.user;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UserRegisterReqDto {
+    private String userId;
+    private String email;
+    private String password;
+    private String passCheck;
+    private String nickname;
+    private LocalDate birthDate;
+    private Character gender;
 }
+
