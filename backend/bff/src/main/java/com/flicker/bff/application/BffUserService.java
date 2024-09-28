4657c858c77928c9e817b63d125fed066431d804
오동규 <ehdrb700606@naver.com>
2024-09-29 00:48:34 +0900

    [feat] Controller, Service 응답 DTO 수정

--- backend/bff/src/main/java/com/flicker/bff/application/BffUserService.java
+++ backend/bff/src/main/java/com/flicker/bff/application/BffUserService.java
@@ -34,7 +34,7 @@ public class BffUserService {
         return util.sendPostRequestAsync(userReviewBaseUrl, path, request);
     }
     // 2. 로그인
-    public Mono<ResponseEntity<ResponseEntity<ResponseDto>>> loginUser(UserLoginReqDto request) {
+    public Mono<ResponseEntity<ResponseDto>> loginUser(UserLoginReqDto request) {
         // 1. 외부 API의 경로를 설정합니다.
         String path = util.getUri("/login");
         // 2. POST 요청을 비동기적으로 외부 API에 보냅니다.
