4d95f456a31d167a9c9ec584a7fc916cd0993fe7
jaeyong <wodyddldl333@naver.com>
2024-10-01 02:20:05 +0900

    [feat] 감성분석 스케쥴링 구현 완료

---
+++ backend/logger/src/main/java/com/flicker/logger/service/SentimentAnalysisService.java
@@ -0,0 +1,40 @@
+package com.flicker.logger.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flicker.logger.dto.SentimentResult;
+import com.flicker.logger.dto.SentimentReviewEvent;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+public class SentimentAnalysisService {
+
+    private final WebClient webClient;
+
+    public SentimentAnalysisService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://70.12.130.131:8000").build();
+    }
+
+    public List<SentimentResult> batchAnalyze(List<SentimentReviewEvent> reviews) {
+
+        try {
+            // 요청 데이터를 JSON으로 직렬화하여 출력
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonString = objectMapper.writeValueAsString(reviews);
+            System.out.println("Request JSON: " + jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return webClient.post()
+                .uri("/sentiment_score")
+                .body(Mono.just(reviews), List.class)
+                .retrieve()
+                .bodyToFlux(SentimentResult.class)
+                .collectList()
+                .block();
+    }
+}
