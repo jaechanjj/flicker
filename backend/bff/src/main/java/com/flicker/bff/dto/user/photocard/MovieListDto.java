bddd75c2adc77a7d42be35e57e2506f2bfa41d0b
오동규 <ehdrb700606@naver.com>
2024-09-30 14:46:19 +0900

    [feat] 포토 카드 조회

---
+++ backend/bff/src/main/java/com/flicker/bff/dto/user/photocard/MovieListDto.java
@@ -0,0 +1,13 @@
+package com.flicker.bff.dto.user.photocard;
+
+import lombok.Data;
+
+@Data
+public class MovieListDto {
+    private int movieSeq;
+
+    private String movieTitle;
+
+    private String moviePosterUrl;
+
+}
