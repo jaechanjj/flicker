a2a053c98468ac965d866ce4f660bcaca6350206
izgnok <rinch12332@gmail.com>
2024-10-02 00:37:04 +0900

    [feat] DTO들 수정

--- backend/bff/src/main/java/com/flicker/bff/dto/movie/ActorResponse.java
+++ backend/bff/src/main/java/com/flicker/bff/dto/movie/ActorResponse.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class ActorResponse {
+    private int actorSeq; // 배우 시퀀스
+
     private String actorName; // 배우 이름
 
     private String role; // 영화에서 배우가 맡은 역할
--- backend/bff/src/main/java/com/flicker/bff/dto/movie/WordCloudResponse.java
+++
@@ -1,9 +0,0 @@
-package com.flicker.bff.dto.movie;
-
-import lombok.Data;
-
-@Data
-public class WordCloudResponse {
-    private String keyword;
-    private int count;
-}
