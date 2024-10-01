a02ef51af5bcc07bf2faa8ddd6fe4ae49efa2534
izgnok <rinch12332@gmail.com>
2024-10-02 01:11:22 +0900

    [fix] actorResponse 수정, 영화상세조회 로그 삭제

--- backend/movie/src/main/java/com/flicker/movie/movie/dto/ActorResponse.java
+++ backend/movie/src/main/java/com/flicker/movie/movie/dto/ActorResponse.java
@@ -5,11 +5,14 @@ import lombok.Data;
 
 @Data
 public class ActorResponse {
+    private int actorSeq; // 배우 ID
+
     private String actorName; // 배우 이름
 
     private String role; // 영화에서 배우가 맡은 역할
 
     public ActorResponse(Actor actor) {
+        this.actorSeq = actor.getActorSeq();
         this.actorName = actor.getActorName();
         this.role = actor.getRole();
     }
