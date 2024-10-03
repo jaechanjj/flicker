a0a39881eea1f12b0a598576a3ec29466109d82d
오동규 <ehdrb700606@naver.com>
2024-10-03 15:39:32 +0900

    [feat] myPage에서 작성한 리뷰 수, 리뷰 좋아요 수 응답 API 작성 및 로그 삭제

--- backend/user/src/main/java/com/flicker/user/review/infrastructure/ReviewRepository.java
+++ backend/user/src/main/java/com/flicker/user/review/infrastructure/ReviewRepository.java
@@ -2,14 +2,19 @@ package com.flicker.user.review.infrastructure;
 
 import com.flicker.user.review.domain.entity.Review;
 import com.flicker.user.review.dto.ReviewRatingCountDto;
+import jakarta.persistence.Tuple;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Integer> {
+
+    @Query("SELECT COALESCE(SUM(r.likes), 0), COUNT(r) FROM Review r WHERE r.userSeq = :userSeq")
+    Tuple findLikesSumAndReviewCountByUserSeq(@Param("userSeq") Integer userSeq);
 
     public Review findByUserSeqAndMovieSeq(Integer userSeq, Integer movieSeq);
     public List<Review> findAllByMovieSeq(Integer movieSeq);
