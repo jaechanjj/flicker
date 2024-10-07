33500079e1a03ee90bf6d29970ac0a8666f9afc6
rinch12332 <rinch12332@gmail.com>
2024-10-07 15:42:45 +0900

    [feat] 로그추가

--- backend/movie/src/main/java/com/flicker/movie/movie/dto/RecommendMovieListRequest.java
+++ backend/movie/src/main/java/com/flicker/movie/movie/dto/RecommendMovieListRequest.java
@@ -1,10 +1,12 @@
 package com.flicker.movie.movie.dto;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
 @Data
+@ToString
 public class RecommendMovieListRequest {
     private List<MovieSeqListRequest> movieSeqListRequest;
     private List<Integer> unlikeMovieSeqList;
