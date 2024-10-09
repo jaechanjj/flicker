30190700aa063430ef0d99df23b8440ea3683cd4
jaeyong <wodyddldl333@naver.com>
2024-10-09 13:58:10 +0900

    [fix] cors 설정

--- backend/user/src/main/java/com/flicker/user/common/config/WebConfig.java
+++ backend/user/src/main/java/com/flicker/user/common/config/WebConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")  // 모든 경로에 대해
-//                .allowedOrigins("http://192.168.30.180:5173", "http://localhost:5173", "http://j11e206.p.ssafy.io:5173")  // 특정 IP와 포트 허용
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")  // 모든 헤더 허용
-//                .exposedHeaders("Authorization")
-//                .allowCredentials(true);  // 자격 증명(쿠키 등) 허용
-//    }
-//}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해
+                .allowedOrigins("https://j11e206.p.ssafy.io")  // 특정 IP와 포트 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);  // 자격 증명(쿠키 등) 허용
+    }
+}
