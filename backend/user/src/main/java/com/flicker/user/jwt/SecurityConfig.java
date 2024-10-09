9da9ccf75bc79c015718205d385567f20e939eeb
이재찬 <jaechanjj@naver.com>
2024-10-09 20:32:20 +0900

    [user] jwt/SecurityConfig import 추가

--- backend/user/src/main/java/com/flicker/user/jwt/SecurityConfig.java
+++ backend/user/src/main/java/com/flicker/user/jwt/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
+
 
 @Configuration
 @EnableWebSecurity
