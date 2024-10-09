f36fe518bb6f3b18bfd0ab9f3c0b416cfce34bf8
jaeyong <wodyddldl333@naver.com>
2024-10-09 11:20:49 +0900

    [fix] gateway bff 전체 경로 허용

--- backend/gateway/src/main/java/com/flicker/gateway/jwt/RouteValidator.java
+++ backend/gateway/src/main/java/com/flicker/gateway/jwt/RouteValidator.java
@@ -10,22 +10,23 @@ import java.util.function.Predicate;
 public class RouteValidator {
 
     public static final List<String> openApiEndPoints = List.of(
-            "/api/bff/user",
-            "/api/bff/user/",
-            "/api/bff/user/login"
+//            "/api/bff/user",
+//            "/api/bff/user/",
+//            "/api/bff/user/login"
+            "/api/bff"
     );
 
     /*
     * 요청 경로가 해당 엔드포린트와 하위 URL을 하용하는 코드
     * */
-//    public Predicate<ServerHttpRequest> isSecured =
-//            request -> openApiEndPoints
-//                    .stream()
-//                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
-
-    // 요청 경로가 허용된 엔드포인트와 정확히 일치하는지 확인
     public Predicate<ServerHttpRequest> isSecured =
             request -> openApiEndPoints
                     .stream()
-                    .noneMatch(uri -> request.getURI().getPath().equals(uri));
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+    // 요청 경로가 허용된 엔드포인트와 정확히 일치하는지 확인
+//    public Predicate<ServerHttpRequest> isSecured =
+//            request -> openApiEndPoints
+//                    .stream()
+//                    .noneMatch(uri -> request.getURI().getPath().equals(uri));
 }
