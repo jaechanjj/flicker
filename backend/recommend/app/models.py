4ce8a79979d54b57835518eaeb44d75eb1b66633
jaeyong <wodyddldl333@naver.com>
2024-10-10 20:25:27 +0900

    [feat] 최종 추천 FastAPI 서버

--- backend/recommend/app/models.py
+++ backend/recommend/app/models.py
@@ -1,29 +1,37 @@
 from pydantic import BaseModel
 from typing import Optional
+from typing import List
+
 
 class SentimentReviewEvent(BaseModel):
     reviewSeq: int
     content: str
 
+
 class SentimentResult(BaseModel):
     reviewSeq: int
     sentimentScore: float
 
+
 class ContentMovieRequest(BaseModel):
     movieTitle: Optional[str] = None  # 생략되면 None이 기본값
-    year: Optional[int] = None        # 생략되면 None이 기본값
-    actorName: Optional[str] = None   # 생략되면 None이 기본값
+    year: Optional[int] = None  # 생략되면 None이 기본값
+    actorName: Optional[str] = None  # 생략되면 None이 기본값
+
 
 class ContentMovieResponse(BaseModel):
     movieTitle: str
     movieYear: int
 
+
 class CollaboMovieResponse(BaseModel):
     movieTitle: str
     movieYear: int
 
+
 class CollaboMovieRequest(BaseModel):
     userSeq: int
+
 
 # 모델 정의
 class ModelUpdateRequest(BaseModel):
@@ -34,6 +42,19 @@ class ModelUpdateRequest(BaseModel):
     rating: float
     sentimentScore: Optional[float] = None
 
+
 class WordCloudRequest(BaseModel):
     movieSeq: int
     content: str
+
+
+class ActorRequest(BaseModel):
+    actorName: str
+
+
+class NewMovieUpdateRequest(BaseModel):
+    movieSeq: int
+    movieTitle: str
+    genre: str
+    movieYear: int
+    actors: List[ActorRequest]
